297e72cb885ac962a55b267c9f9697d4f2001862
nikaarabuli <nikoloz.arabuli.1@btu.edu.ge>
2022-08-30 18:42:26 +0400

    new functions

--- newcode.js
+++ newcode.js
@@ -54,5 +54,99 @@ alert ("if your dog is" + " " + dogage + " " + "years old, then you must be" + "
 
 /* JS FUNCTIONS */
 
+function WelcometoClient () {
 
- 
+var clientname= prompt ("dear customer, What's your name?");
+var firstletter=clientname.slice(0,1);
+var remainingletters=clientname.slice(1,clientname.length);
+firstletter=firstletter.toUpperCase();
+alert ("welcome dear" + " " + firstletter + remainingletters);
+
+}
+
+WelcometoClient();
+
+/* */
+
+function DogToHumanAge() {
+
+var dogage=prompt ("what is your dog's age?")
+var humanage= (dogage -2) * 4 + 21;
+alert ("if your dog is" + " " + dogage + " " + "years old, then you must be" + " " + humanage + " " + "years old");
+}
+
+DogToHumanAge();
+
+/*  you can create several functions and unite them under another function and call all of them by just calling this one function*/
+
+/*e.g. */
+
+function functioncombination() {
+WelcometoClient();
+DogToHumanAge();
+}
+
+functioncombination();
+
+/* Task: you got 340$ to buy several pairs of shoes: unit price is 90$. create variables to know how many pairs you can buy and how much money you'll have remaining*/
+var sum=340;
+var price=90;
+var numberofshoes=(sum/price);
+console.log(math.floor (numberofshoes) );
+
+var sum=340;
+var price=90;
+var remainingmoney=sum%price;
+console.log (remainingmoney);
+
+/* Task: imagine you'll leave till 90 years. create a functions that takes your age and returns years months, weeks, days remaining till you die */
+
+function monthsremaining () {
+
+var age=prompt ("How old are you?");
+var remainingmonths= (90-age)*12;
+return remainingmonths;
+
+}
+monthremaining();
+
+function daysremaining() {
+var age=prompt ("How old are you?")
+var daysremaining=(90-age) * 365;
+return daysremaining;
+
+}
+daysremaining();
+
+/* or you can write same function this way:*/
+function timeremaining(age) {
+
+var monthremained=(90-age) * 12;
+var weekremained=(90-age) * 52;
+var daysremained=(90-age) * 365;
+console.log("you have" + " " + monthremained + " " + "months" + " " + weekremained + " " + "weeks and" + " " + daysremained + " " + "days remained.");
+}
+timeremaining(24);
+
+/* Task: write BMI calculator. in BMI calculator you got input: weight and height. output is your body mass index which is calculated
+from weight devided by height in square.
+*/
+
+function BMIcalculator (weight, height) {
+
+var BMI=Math.floor (weight/(height*height));
+return BMI;
+
+}
+BMIcalculator (80, 188);
+
+/* Task: create a calclator that calculates change after paying for something */
+
+function changecalculator (sum,price) {
+
+var change=sum%price;
+return change;
+}
+changecalculator(400,356);
+
+/*  var randomnumber=Math.random(); - by this you get random number */
