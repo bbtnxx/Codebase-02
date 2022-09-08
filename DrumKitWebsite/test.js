351b2c9bbf90696200cd834c625a774b2894d87e
Malkhaz Arabuli <malkhazarabuli@Malkhazs-MacBook-Air.local>
2022-09-08 13:01:03 +0400

    update learningJS file

---
+++ DrumKitWebsite/test.js
@@ -0,0 +1,14 @@
+function play () {
+  var audio = new Audio("sounds/kick-bass.mp3");
+  audio.play();
+};
+
+document.querySelector(".w").addEventListener ("click", play);
+
+// a button
+function play () {
+  var audio = new Audio("sounds/tom-2.mp3");
+  audio.play();
+};
+
+document. querySelector (".a").addEventListener ("click", play);
