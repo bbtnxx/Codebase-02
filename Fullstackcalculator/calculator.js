2b728802b58b40a6f581933f0dca87bc00fb65e8
Malkhaz Arabuli <malkhazarabuli@Malkhazs-MacBook-Air.local>
2022-09-13 17:04:38 +0400

    add 2 fullstack web app

--- Fullstackcalculator/calculator.js
+++ Fullstackcalculator/calculator.js
@@ -1,6 +1,9 @@
-const express = require("express");
-const app = express();
+const express = require("express"); // this way you incorporate package to a project
+const bodyParser = require ("body-parser"); // without this package, we can't parse the values of post method. this package allows
+// us to go into post routes ("/") and  parse the passed values
 
+const app = express(); // this way you start using a package in a project
+app.use (bodyParser.urlencoded({extended: true})); // body-parser works with express so there's no need to create from scratch
 
 app. get ("/", function (request,response) {
 response.sendFile(__dirname + "/index.html");
@@ -10,7 +13,36 @@ app. get ("/about", function (request,response) {
 response.send ("This is Max, World!");
 });
 
+app.post ("/", function (request, response) {
+// console.log(request.body); - this is body-parse package. reques.body - this way it logs entire body of html. or you can tap into object properties and say: request.body.num1
 
-app.listen (8083, function (){
+var num1 = Number (request.body.num1);  // you put Number in front of var. otherwise javascript will read this values as strings and just adds to each other e.g 1+4 = 14
+var num2 = Number (request.body.num2);
+var result= num1+num2;
+
+response.send ("the result of the calculation is" + " " + result);
+});
+
+
+// BMI calculator
+
+app. get ("/bmicalculator", function (request,response) {
+response.sendFile(__dirname + "/bmicalculator.html");
+});
+
+app.post ("/bmicalculator", function (request, response) {
+console.log(request.body);
+
+var weight = parseFloat(request.body.weight);
+var height = parseFloat(request.body.height);
+
+var bmi = Math.floor (weight/(height*height));
+
+response. send ("your body max index is" + " " + bmi);
+
+});
+
+
+app.listen (8022, function (){
   console.log("calculator started on port 8083");
 });
--- NodeJSintro/learningNode.js
+++ NodeJSintro/learningNode.js
@@ -19,4 +19,27 @@ const express = require ("express");
 const app = express();
 app.listen (8080);
 
-// then you open a terminal and run server.js file. from browser you can get into server: localhost:8080 - like this. 
+// then you open a terminal and run server.js file. from browser you can get into server: localhost:8080 - like this.
+
+
+//           SETT UP SERVER AND WRITE ENDPOINTS
+
+1. npm init // this is a command in a terminal to create package.json file. after you run this command, you need to click enter's and change some parameters if you wish.
+2. npm install express  // then you need to install specific package via terminal
+3. //and then follow steps:
+const express = require("express"); //then you require this package from editor.
+const app = express();
+
+
+app. get ("/", function (request,response) {
+response.sendFile(__dirname + "/index.html");
+}); // instead of a string like below, here we send a html file. now we want to write post endpoint
+
+app. get ("/about", function (request,response) {
+response.send ("This is Max, World!");
+});
+
+
+app.listen (8083, function (){
+  console.log("calculator started on port 8083");
+});
